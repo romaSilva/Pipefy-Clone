d60642567eec63e24accb9e5d25a94bcb188b446
Romario Da Silva <roma.dasilva94@gmail.com>
2020-06-08 15:18:56 -0300

    basic functionalities implemented

---
+++ src/components/List/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "../Card";
+import { Container } from "./styles";
+import { MdAdd } from "react-icons/md";
+
+const List = ({ data, index: listIndex }) => {
+  return (
+    <Container done={data.done}>
+      <header>
+        <h2>{data.title}</h2>
+        {data.creatable && (
+          <button type="button">
+            <MdAdd size={24} color="#FFF" />
+          </button>
+        )}
+      </header>
+
+      <ul>
+        {data.cards.map((card, index) => (
+          <Card key={card.id} listIndex={listIndex} data={card} index={index} />
+        ))}
+      </ul>
+    </Container>
+  );
+};
+
+export default List;
